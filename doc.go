2ac7fdac656bbfba2c9ecf0a8c7885d1c7ac01ca
Tim O'Brien <tim@tekao.net>
2013-03-11 23:46:54 +1300

    improve docs

--- doc.go
+++ doc.go
@@ -1,11 +1,16 @@
 /*
 Httpize is a web framework, that allows you use methods/types in web requests.
 
-A value that has a method with a correct signature that can be exported so that,
-the method will be called on the value for a HTTP request. This is achieved by:
-Using AddType to add parameter types to be created by the handler from the URL of a HTTP request to be passed to a method.
-Using Export to export methods that will be called by the the handler.
-Using NewHandler to create a Handler for a value that has exported methods. The Handler is then passed
-to http.Handle in the normal way.
+A method can be exported so the method will be called by a handler for a HTTP request. 
+This is achieved by:
+
+1) Using AddType() to add value creating functions for a type, so values for typed arguments to methods can 
+be created from request URL parameters.
+
+2) Using Export() to export methods that will be called by the the handler.
+
+3) Using NewHandler() to create a Handler for a value that has exported methods.
+
+The Handler is then passed to http.Handle in the normal way.
 */
 package httpize
